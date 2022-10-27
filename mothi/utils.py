d6c89c13e52fb23b12b1f29c51320f2dff4cbdf7
Thomas Kauer <thomas-kauer@web.de>
2022-10-27 18:46:24 +0200

    apply typing

--- mothi/utils.py
+++ mothi/utils.py
@@ -1,83 +1,96 @@
+from typing import Any, List, Tuple, Union
+
 import numpy as np
+from numpy.typing import NDArray
 import cv2
 import shapely
 
 from shapely.validation import make_valid
 from shapely.geometry import Polygon, CAP_STYLE, JOIN_STYLE
+from shapely.geometry.base import BaseGeometry
+from shapely.geometry.polygon import InteriorRingSequence
 
 
-def label_img_to_polys(label_img, downsample_level = 0, min_polygon_area = 0, multilabel = False):
+def label_img_to_polys(label_img: NDArray[np.uint8],
+        downsample_level: int = 0,
+        min_polygon_area: Union[float, int] = 0,
+        multilabel: bool = False) -> List[Tuple[Union[Polygon, BaseGeometry], int]]:
     ''' convert label mask to list of Polygons
 
     Parameters
     ----------
         label_img:
-            mask [H, W] with values between 0 and highest label class
+            mask [height, width] with an annotation class for each pixel
+            or [num_class, height, width] for multilabels
+            background class is ignored for multilabels
         downsample_level:
             level for downsampling
         min_polygon_area:
             minimal area for polygons to be saved
         multilabel:
-            if True annotation mask contains boolean image for each class ([num_classes, width, height])
-
+            if True annotation mask contains boolean image for each class
     Returns
     -------
-        poly_labels: _ 
-            list of Polygon and label tuple [(polygon, label), ...]
+        :
+            list of Polygon and label number tuple [(polygon, label_nmbr), ...]
     '''
-    downsample_factor = 2 ** downsample_level
-    label_img = label_img.astype(np.uint8)
-    poly_labels = []
+    downsample_factor: int = 2 ** downsample_level
+    poly_labels: List[Tuple[Union[Polygon, BaseGeometry], int]] = []
 
-    if multilabel:
-        iter_range = range(len(label_img))
-    else:
-        iter_range = range(1, np.max(label_img)+1)
+    iter_range: range = range(len(label_img)) if multilabel else range(1, np.max(label_img) + 1)
 
-    for i in iter_range:
+    class_id: int
+    for class_id in iter_range:
+        it_img: NDArray[np.uint8]
         if multilabel:
-            it_img = label_img[i]
-            i += 1
+            it_img = label_img[class_id]
+            class_id += 1
         else:
-            label_img_copy = label_img.copy()
+            label_img_copy: NDArray[np.uint8] = label_img.copy()
+            it_img = np.where(label_img_copy == class_id, 1, 0).astype(np.uint8)
 
-            it_img = np.where(label_img_copy == i, 1, 0).astype(np.uint8)
-            
+        contours: Tuple[NDArray[np.int32], ...]
+        hierarchy: NDArray[np.int32]
         contours, hierarchy = cv2.findContours(it_img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-        contours = list(map(np.squeeze, contours))
+        contours = tuple(map(np.squeeze, contours))
         if len(contours) == 0:
             continue
         # iterate hierarchy
-        next_id = 0                                 # start with first contour
+        next_id: int = 0                                 # start with first contour
         while next_id != -1:
-            current_id = next_id                    # previous next id is now our current id
+            current_id: int = next_id                    # previous next id is now our current id
+            child_id: int = hierarchy[0][current_id][2]
             next_id = hierarchy[0][current_id][0]
-            child_id = hierarchy[0][current_id][2]
 
             if len(contours[current_id]) < 3:       # linear ring possible?
                 continue
-            
+
+            # if contour has childs, thoose are the holes
+            # -1 -> no childs
+            poly: Union[Polygon, BaseGeometry] # final Polygon per iteration
             if child_id == -1:
                 poly = Polygon(contours[current_id])
                 poly = Polygon(*_round_polygon(poly, export = False))
             else:
-                holes = []
-                hole_poly = Polygon(contours[child_id]).buffer(
+                holes: List[NDArray[np.int32]] = []
+                hole_poly: Polygon = Polygon(Polygon(contours[child_id]).buffer(
                     -1,
                     join_style= JOIN_STYLE.mitre,
                     cap_style= CAP_STYLE.square
-                )
+                ))
                 holes.append(list(map(
                     lambda coords: np.array(coords).round().astype(np.int32),
                     hole_poly.exterior.coords
                 )))
-                next_child_id = hierarchy[0][child_id][0]
+                # search for further childs
+                # further childs are listed by next in hierarchy of a known child
+                next_child_id: int = hierarchy[0][child_id][0]
                 while next_child_id != -1:
-                    hole_poly = Polygon(contours[child_id]).buffer(
+                    hole_poly = Polygon(Polygon(contours[child_id]).buffer(
                         -1,
                         join_style= JOIN_STYLE.mitre,
                         cap_style= CAP_STYLE.square
-                    )
+                    ))
                     holes.append(list(map(
                         lambda coords: np.array(coords).round().astype(np.int32),
                         hole_poly.exterior.coords
@@ -87,16 +100,19 @@ def label_img_to_polys(label_img, downsample_level = 0, min_polygon_area = 0, mu
                 poly = Polygon(*_round_polygon(poly, export = False))
 
             poly = shapely.affinity.scale(poly, xfact = 1*downsample_factor, yfact = 1*downsample_factor, origin = (0,0))
+                                          xfact = 1 * downsample_factor,
+                                          yfact = 1 * downsample_factor,
+                                          origin = (0,0))
             if not poly.is_valid:
                 poly = make_valid(poly)
             if poly.area > min_polygon_area:
-                poly_labels.append((poly, i))
+                poly_labels.append((poly, class_id))
 
     return poly_labels
 
 
 def _round_polygon(polygon: Polygon, export = True):
-    ''' round polygon coords to discrete values 
+    ''' round polygon coords to discrete values
 
     Parameters
     ----------
@@ -110,9 +126,9 @@ def _round_polygon(polygon: Polygon, export = True):
         :
             rounded interior coords
     '''
-    exteriors: NDArray[np.int32] = np.array(polygon.exterior.coords) # type: ignore
+    exteriors: NDArray[np.int32] = np.array(polygon.exterior.coords)
     centroid_coords: NDArray[np.float64] = np.array(
-        [polygon.centroid.x, polygon.centroid.y],  # type:ignore
+        [polygon.centroid.x, polygon.centroid.y],
         dtype=np.float64
     )
 
@@ -153,7 +169,7 @@ def _int_coord(coord: np.float64, centroid_coords: np.float64, export: bool) -> 
     export:
         differ between import and export: increase or decrease coordinate
         depends on relation to centroid coordinate
-    
+
     Returns
     -------
     :
@@ -161,12 +177,12 @@ def _int_coord(coord: np.float64, centroid_coords: np.float64, export: bool) -> 
     '''
     if export:
         return np.where(
-        coord > centroid_coords,
-        np.round(coord) - 1,
-        np.round(coord)
+            coord > centroid_coords,
+            np.round(coord) - 1,
+            np.round(coord)
         )
     return np.where(
         coord > centroid_coords,
         np.round(coord) + 1,
         np.round(coord)
-        )
+    )
