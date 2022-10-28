cdfca1fd1297f4f28c9fc7918c466dc0fca971a6
thkauer <thomas-kauer@web.de>
2022-10-28 13:36:01 +0000

    rewrite and add documentation (python)

--- mothi/utils.py
+++ mothi/utils.py
@@ -13,64 +13,76 @@ from shapely.geometry.polygon import InteriorRingSequence
 def label_img_to_polys(label_img: NDArray[np.uint8],
         downsample_level: int = 0,
         min_polygon_area: Union[float, int] = 0,
-        multilabel: bool = False) -> List[Tuple[Union[Polygon, BaseGeometry], int]]:
-    ''' convert label mask to list of Polygons
+        multichannel: bool = False) -> List[Tuple[Union[Polygon, BaseGeometry], int]]:
+    ''' convert label mask to a list of Polygons
 
     Parameters
     ----------
         label_img:
-            mask [height, width] with an annotation class for each pixel
-            or [num_class, height, width] for multilabels
+            mask [height, width] with an annotation class for each pixel \n
+            or [num_class, height, width] for multilabels \n
             background class is ignored for multilabels
         downsample_level:
             level for downsampling
         min_polygon_area:
-            minimal area for polygons to be saved
-        multilabel:
-            if True annotation mask contains boolean image for each class
+            minimal polygon area to save polygon
+        multichannel:
+            True: binary image input [num_channels, height, width] \n
+            False: labeled image input [height, width]
     Returns
     -------
         :
-            list of Polygon and label number tuple [(polygon, label_nmbr), ...]
+            list of detected polygons with their annotationclass number [(polygon, annot_cls), ...]
     '''
     downsample_factor: int = 2 ** downsample_level
     poly_labels: List[Tuple[Union[Polygon, BaseGeometry], int]] = []
 
-    iter_range: range = range(len(label_img)) if multilabel else range(1, np.max(label_img) + 1)
+    # range up to the highest annotated class id
+    iter_range: range = range(len(label_img)) if multichannel else range(1, np.max(label_img) + 1)
 
     class_id: int
     for class_id in iter_range:
+        # get the binary mask of the current class
         it_img: NDArray[np.uint8]
-        if multilabel:
+        if multichannel:
             it_img = label_img[class_id]
             class_id += 1
         else:
             label_img_copy: NDArray[np.uint8] = label_img.copy()
             it_img = np.where(label_img_copy == class_id, 1, 0).astype(np.uint8)
 
+        ## find contours in binary mask
         contours: Tuple[NDArray[np.int32], ...]
         hierarchy: NDArray[np.int32]
         contours, hierarchy = cv2.findContours(it_img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
         contours = tuple(map(np.squeeze, contours))
         if len(contours) == 0:
             continue
-        # iterate hierarchy
-        next_id: int = 0                                 # start with first contour
+        ## iterate the hierarchy of detected contours
+        # start with first contour
+        next_id: int = 0
         while next_id != -1:
-            current_id: int = next_id                    # previous next id is now our current id
+            # previous next id is now our current id
+            current_id: int = next_id
+            # get id of the child contour
             child_id: int = hierarchy[0][current_id][2]
+            # get id of the next contour
             next_id = hierarchy[0][current_id][0]
 
-            if len(contours[current_id]) < 3:       # linear ring possible?
+            # skip if the creation of a linear ring is not possible
+            if len(contours[current_id]) < 3:
                 continue
 
-            # if contour has childs, thoose are the holes
+            # final Polygon per iteration
+            poly: Union[Polygon, BaseGeometry]
+            # if the contour has childs, thoose contours are the holes
             # -1 -> no childs
-            poly: Union[Polygon, BaseGeometry] # final Polygon per iteration
             if child_id == -1:
+                # create polygon without holes
                 poly = Polygon(contours[current_id])
                 poly = Polygon(*_round_polygon(poly, export = False))
             else:
+                ## detect all holes
                 holes: List[NDArray[np.int32]] = []
                 hole_poly: Polygon = Polygon(Polygon(contours[child_id]).buffer(
                     -1,
@@ -82,7 +94,7 @@ def label_img_to_polys(label_img: NDArray[np.uint8],
                     0,
                     hole_poly.exterior.coords  # type: ignore
                 ))
-                # search for further childs
+                ## search for further childs
                 # further childs are listed by next in hierarchy of a known child
                 next_child_id: int = hierarchy[0][child_id][0]
                 while next_child_id != -1:
@@ -97,9 +109,11 @@ def label_img_to_polys(label_img: NDArray[np.uint8],
                         hole_poly.exterior.coords  # type:ignore
                     ))
                     next_child_id = hierarchy[0][next_child_id][0]
+                # create polygon with holes
                 poly = Polygon(contours[current_id], holes)
                 poly = Polygon(*_round_polygon(poly, export = False))
 
+            # scale poly to level 0 (no downsampling) size
             poly = affinity.scale(poly,
                 xfact = 1 * downsample_factor,
                 yfact = 1 * downsample_factor,
@@ -114,22 +128,24 @@ def label_img_to_polys(label_img: NDArray[np.uint8],
     return poly_labels
 
 
-def _round_polygon(polygon: Polygon, export = True):
-    ''' round polygon coords to discrete values
+def _round_polygon(polygon: Polygon,
+        export: bool = True) -> Tuple[NDArray[np.int32], List[NDArray[np.int32]]]:
+    ''' round polygon coordinates to discrete values
 
     Parameters
     ----------
         polygon:
-            Polygon to round coords
+            Polygon whose coordinates are to be rounded
 
     Returns
     -------
         :
             rounded exterior coords
         :
-            rounded interior coords
+            list of rounded interior coords
     '''
     exteriors: NDArray[np.int32] = np.array(polygon.exterior.coords)  # type: ignore
+    ## get centroid of the polygon
     centroid_coords: NDArray[np.float64] = np.array(
         [polygon.centroid.x, polygon.centroid.y],  # type: ignore
         dtype=np.float64
@@ -137,19 +153,23 @@ def _round_polygon(polygon: Polygon, export = True):
 
     discrete_interiors: List[NDArray[np.int32]] = []
     interior_polys: Union[InteriorRingSequence, List[Any]] = polygon.interiors
+    # get centroid of the polygon interiors
     interior_centroids: List[NDArray[np.float64]] = [
         np.array([poly.centroid.x, poly.centroid.y], dtype=np.float64)
         for poly in interior_polys
     ]
+    # get list of the polygon coordinates
     interiors_coord: List[NDArray[np.float64]] = [
         np.array(poly.coords, dtype=np.float64)
         for poly in interior_polys
     ]
 
+    ## round and adjust coordinates
     exteriors = np.apply_along_axis(_int_coord, 1,
                                     exteriors,
                                     centroid_coords,
                                     export).astype(np.int32)
+    coords: NDArray[np.float64]
     if len(interior_polys) > 0:
         for coords, centroid_coords in zip(interiors_coord, interior_centroids):
             discrete_interiors.append(
@@ -158,10 +178,12 @@ def _round_polygon(polygon: Polygon, export = True):
     return exteriors, discrete_interiors
 
 
-def _int_coord(coord: np.float64, centroid_coords: np.float64, export: bool) -> NDArray[np.int32]:
-    ''' round coordinate,
-        increase or decrease value in comparision to the centroid
-        (needed to eliminate difference between QuPath and shapely)
+def _int_coord(coord: NDArray[np.float64],
+        centroid_coords: NDArray[np.float64],
+        export: bool) -> NDArray[np.int32]:
+    ''' round coordinate \n
+    increase or decrease value in comparision to the centroid
+    (needed to eliminate difference between QuPath and shapely)
 
     Parameters
     ----------
@@ -170,13 +192,13 @@ def _int_coord(coord: np.float64, centroid_coords: np.float64, export: bool) -> 
     centroid_coords:
         reference centroid coordinate
     export:
-        differ between import and export: increase or decrease coordinate
+        differ between import and export: increase or decrease coordinate \n
         depends on relation to centroid coordinate
 
     Returns
     -------
     :
-        [rounded and] adjusted Array
+        (rounded and) adjusted Array
     '''
     if export:
         return np.where(
