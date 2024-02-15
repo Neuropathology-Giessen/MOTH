77ddb5019f759daeda895fd3b40338ac62bcb9bf
thkauer <thomas-kauer@web.de>
2024-02-15 14:45:47 +0000

    Add groovy export script

---
+++ paper/comparison_groovy/export_annotations.groovy
@@ -0,0 +1,23 @@
+import qupath.lib.images.servers.LabeledImageServer
+
+def imageData = getCurrentImageData()
+
+def name = GeneralTools.getNameWithoutExtension(imageData.getServer().getMetadata().getName())
+def pathOutput = buildFilePath('groovy_export', name)
+mkdirs(pathOutput)
+
+
+def labelServer = new LabeledImageServer.Builder(imageData)
+    .backgroundLabel(0, ColorTools.WHITE)
+    .addLabel('Tumor',1)
+    .build()
+    
+new TileExporter(imageData)
+    .imageExtension('.tif')
+    .tileSize(32)
+    .labeledServer(labelServer)
+    .annotatedTilesOnly(false)
+    .writeTiles(pathOutput) 
+    
+    
+print 'Done!'
