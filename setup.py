70e241e9d015cc0490f9797766a11f224a39f885
Thomas Kauer <thomas-kauer@web.de>
2024-04-26 22:27:08 +0000

    Refactor pyproject.toml and setup.py - Update dependencies in pyproject.toml - Remove setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    packages = ['mothi'],
-    install_requires = [
-        'paquo>=0.5.1',
-        'opencv-python>=4.6.0.66',
-        'tiffslide>=1.10.0'
-    ])
